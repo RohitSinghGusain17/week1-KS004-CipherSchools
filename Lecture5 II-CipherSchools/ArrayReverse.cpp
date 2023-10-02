60f97fa86153a55b303cec27ce6eac4cf9a13562
RohitSinghGusain17 <117933189+RohitSinghGusain17@users.noreply.github.com>
2023-10-02 12:15:07 +0530

    ArrayReverse.cpp

--- Lecture5 II-CipherSchools/ArrayReverse.cpp
+++ Lecture5 II-CipherSchools/ArrayReverse.cpp
@@ -1,1 +1,15 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout << "Enter size of array: ";
+	cin >> n;
+	int arr[n]; //Create array of size n with same datatype(int).
+	for(int i=0;i<n;i++){
+		cin >> arr[i]; //taking input in array.
+	}
+	cout << "Reversed array is : ";
+	for(int i=n-1;i>=0;i--){
+		cout << arr[i] << " ";
+	} 
+}
